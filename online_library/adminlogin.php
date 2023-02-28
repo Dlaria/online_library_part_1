ce52a1341562eb08786968f887265c219470f92a
Airald <airald.73@orange.fr>
2023-02-28 11:11:08 +0100

    adminlogin est fonctionnel

--- online_library/adminlogin.php
+++ online_library/adminlogin.php
@@ -12,23 +12,39 @@ if (isset($_SESSION['login']) && $_SESSION['alogin'] != '') {
 
 // A faire :
 // Apres la soumission du formulaire de login (plus bas dans ce fichier)
-// On verifie si le code captcha est correct en comparant ce que l'utilisateur a saisi dans le formulaire
-// $_POST["vercode"] et la valeur initialis�e $_SESSION["vercode"] lors de l'appel a captcha.php (voir plus bas
+if (true === isset($_POST['login'])){
+    // On verifie si le code captcha est correct en comparant ce que l'utilisateur a saisi dans le formulaire
+    // $_POST["vercode"] et la valeur initialis�e $_SESSION["vercode"] lors de l'appel a captcha.php (voir plus bas
+    if ($_POST['vercode'] != $_SESSION['vercode']){
+        echo "<script>alert('Code de vérification incorrect')</script>";
+    }else{
+        // Le code est correct, on peut continuer
+        // On recupere le nom de l'utilisateur saisi dans le formulaire
+        $name = $_POST['name'];
+        // On recupere le mot de passe saisi par l'utilisateur et on le crypte (fonction md5)
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// Le code est correct, on peut continuer
-// On recupere le nom de l'utilisateur saisi dans le formulaire
+        // On construit la requete qui permet de retrouver l'utilisateur a partir de son nom et de son mot de passe
+        // depuis la table admin
+        $sql = "SELECT UserName, Password FROM admin WHERE UserName=:name";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':name',$name,PDO::PARAM_STR);
+        $query->execute();
 
-// On recupere le mot de passe saisi par l'utilisateur et on le crypte (fonction md5)
+        $result = $query->fetch(PDO::FETCH_OBJ);
 
-// On construit la requete qui permet de retrouver l'utilisateur a partir de son nom et de son mot de passe
-// depuis la table admin
+        if (!empty($result) && password_verify($_POST['password'], $result->Password)){
+            // Si le resultat de recherche n'est pas vide 
+            // On stocke le nom de l'utilisateur  $_POST['username'] en session $_SESSION
+            // On redirige l'utilisateur vers le tableau de bord administration (n'existe pas encore)
+            header('location:admin.php');
 
-// Si le resultat de recherche n'est pas vide 
-// On stocke le nom de l'utilisateur  $_POST['username'] en session $_SESSION
-// On redirige l'utilisateur vers le tableau de bord administration (n'existe pas encore)
-
-// sinon le login est refuse. On le signal par une popup
-
+        }else{
+            // sinon le login est refuse. On le signal par une popup
+            echo "<script>alert('Utilisateur inconnu')</script>";
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -52,10 +68,32 @@ if (isset($_SESSION['login']) && $_SESSION['alogin'] != '') {
 
     <div class="content-wrapper">
         <!--On affiche le titre de la page-->
-
+        <div class="row">
+			<div class="col">
+                <h3>ADMIN LOGIN</h3>
+			</div>
+		</div>
         <!--On affiche le formulaire de login-->
-
-        <!--A la suite de la zone de saisie du captcha, on ins�re l'image cr��e par captcha.php : <img src="captcha.php">  -->
+        <div class="row">
+		    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-8 offset-md-3">
+                <form action="adminlogin.php" method="post">
+                <div class="form-group">
+                    <label for="name">Entrez votre nom</label>
+                    <input type="text" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" required>
+                </div>
+                <div class="form-group">
+                    <!--A la suite de la zone de saisie du captcha, on ins�re l'image cr��e par captcha.php : <img src="captcha.php">  -->
+                    <label for="vercode">Code de vérification</label>
+                    <input type="text" name="vercode" required style="height:25px;">&nbsp;&nbsp;&nbsp;<img src="captcha.php">
+                </div>
+                <input type="submit" name="login" id="btnSubmit" class="btn btn-info" value ="Login"/>
+                </form>
+            </div>
+        </div>
     </div>
     <!-- CONTENT-WRAPPER SECTION END-->
     <?php include('includes/footer.php'); ?>
