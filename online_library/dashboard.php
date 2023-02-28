6df7cf4e3185c26acf15ac4474350f945d2d5160
Airald <airald.73@orange.fr>
2023-02-28 15:05:25 +0100

    dashboard est fonctionnel

--- online_library/dashboard.php
+++ online_library/dashboard.php
@@ -2,27 +2,38 @@
 // On recupere la session courante
 session_start();
 
-// On inclue le fichier de configuration et de connexion a la base de donnÈes
+// On inclue le fichier de configuration et de connexion a la base de donnÔøΩes
 include('includes/config.php');
 
 
 if(strlen($_SESSION['login'])==0) {
-	// Si l'utilisateur est dÈconnectÈ
-	// L'utilisateur est renvoyÈ vers la page de login : index.php
+	// Si l'utilisateur est dÔøΩconnectÔøΩ
+	// L'utilisateur est renvoyÔøΩ vers la page de login : index.php
   header('location:index.php');
 } else {
-	// On rÈcupËre l'identifiant du lecteur dans le tableau $_SESSION
-	
+	// On rÔøΩcupÔøΩre l'identifiant du lecteur dans le tableau $_SESSION
+	$readerId = $_SESSION['rdid'];
+
 	// On veut savoir combien de livres ce lecteur a emprunte
 	// On construit la requete permettant de le savoir a partir de la table tblissuedbookdetails
-	
-	// On stocke le rÈsultat dans une variable
+     $sql = "SELECT ReaderID FROM tblissuedbookdetails WHERE ReaderID=:readerId";
+     $query = $dbh->prepare($sql);
+     $query->bindParam(":readerId",$readerId,PDO::PARAM_STR);
+     $query->execute();
+
+	// On stocke le rÔøΩsultat dans une variable
+     $result = $query->fetchAll(PDO::FETCH_OBJ);
+	//error_log(var_dump($result));
 	
 	// On veut savoir combien de livres ce lecteur n'a pas rendu
-	// On construit la requete qui permet de compter combien de livres sont associÈs ‡ ce lecteur avec le ReturnStatus ‡ 0 
-	
-	// On stocke le rÈsultat dans une variable
+	// On construit la requete qui permet de compter combien de livres sont associÔøΩs ÔøΩ ce lecteur avec le ReturnStatus ÔøΩ 0 
+	$sql = "SELECT ReturnStatus FROM tblissuedbookdetails WHERE ReaderID=:readerId AND ReturnStatus=0";
+     $query = $dbh->prepare($sql);
+     $query->bindParam(":readerId",$readerId,PDO::PARAM_STR);
+     $query->execute();
 
+	// On stocke le rÔøΩsultat dans une variable
+     $resultNum = $query->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <!DOCTYPE html>
@@ -42,10 +53,28 @@ if(strlen($_SESSION['login'])==0) {
      <!--On inclue ici le menu de navigation includes/header.php-->
 <?php include('includes/header.php');?>
 <!-- On affiche le titre de la page : Tableau de bord utilisateur-->
+     <div class="content-wrapper">
+		<div class="row">
+			<div class="col">
+				<h3>Tableau de bord utilisateur</h3>
+			</div>
+		</div>
+          <div class="fa fa-border" style="text-align:center">
+               <!-- On affiche la carte des livres empruntÔøΩs par le lecteur-->
+               <div class="fa-5x fa-bars"></div>
+               <span name="livreEmprunter" class="fa-2x"><?php echo count($result);?></span>
+               <p class="fa-2x">Livre emprunt√©s</p>
+               
+          </div>
+          <!-- On affiche la carte des livres non rendus le lecteur-->
+          <div class="fa fa-border" style="text-align:center">
+               <div class="fa-5x fa-recycle"></div>
+               <span name="livreNonRendu" class="fa-2x"><?php echo count($resultNum);?></span>
+               <p class="fa-2x">Livre non encore rendu</p>
+          </div>
+	</div>
  
-   <!-- On affiche la carte des livres empruntÈs par le lecteur-->
         
-   <!-- On affiche la carte des livres non rendus le lecteur-->
 
 <?php include('includes/footer.php');?>
      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
