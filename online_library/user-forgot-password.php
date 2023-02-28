1c6cacb0581bdab5cd15e5db91157112ba9f7a14
Airald <airald.73@orange.fr>
2023-02-28 10:16:37 +0100

    Création du changement de mot de passe

--- online_library/user-forgot-password.php
+++ online_library/user-forgot-password.php
@@ -4,21 +4,48 @@ session_start();
 
 // On inclue le fichier de configuration et de connexion à la base de données
 include('includes/config.php');
-// Après la soumission du formulaire de login ($_POST['change'] existe
-// On verifie si le code captcha est correct en comparant ce que l'utilisateur a saisi dans le formulaire
-// $_POST["vercode"] et la valeur initialisee $_SESSION["vercode"] lors de l'appel a captcha.php (voir plus bas)
+// Après la soumission du formulaire de login $_POST['login'] existe
+if (true === isset($_POST['login'])){
+     // On verifie si le code captcha est correct en comparant ce que l'utilisateur a saisi dans le formulaire
+     // $_POST["vercode"] et la valeur initialisee $_SESSION["vercode"] lors de l'appel a captcha.php (voir plus bas)
+     if ($_POST['vercode'] != $_SESSION['vercode']) {
+          // Si le code est incorrect on informe l'utilisateur par une fenetre pop_up
+          echo "<script>alert('Code de vérification incorrect')</script>";
+     } else {
+          // Sinon on continue
+          // on recupere l'email et le numero de portable saisi par l'utilisateur
+          // et le nouveau mot de passe que l'on encode (fonction password_hash)
+          $email = $_POST['email'];
+          $mobileNum = $_POST['portable'];
+          $newPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// Si le code est incorrect on informe l'utilisateur par une fenetre pop_up
+          // On cherche en base le lecteur avec cet email et ce numero de tel dans la table tblreaders
+          $sql = "SELECT EmailId, MobileNumber FROM tblreaders WHERE EmailId=:email AND MobileNumber=:mobileNum";
+          $query = $dbh->prepare($sql);
+          $query->bindParam(":email", $email,PDO::PARAM_STR);
+          $query->bindParam(":mobileNum", $mobileNum,PDO::PARAM_INT);
+          $query->execute();
 
-// Sinon on continue
-// on recupere l'email et le numero de portable saisi par l'utilisateur
-// et le nouveau mot de passe que l'on encode (fonction password_hash)
+          // On stocke le resultat de recherche dans une variable $result
+          $result = $query->fetch(PDO::FETCH_OBJ);
 
-// On cherche en base le lecteur avec cet email et ce numero de tel dans la table tblreaders
+          if(!empty($result)){
+               // Si le resultat de recherche n'est pas vide
+               // On met a jour la table tblreaders avec le nouveau mot de passe
+               // On informa l'utilisateur par une fenetre popup de la reussite ou de l'echec de l'operation
+               $sql = "UPDATE tblreaders SET Password=:newPassword WHERE EmailId=:email";
+               $query = $dbh->prepare($sql);
+               $query->bindParam(":email", $email,PDO::PARAM_STR);
+               $query->bindParam(":newPassword", $newPassword,PDO::PARAM_STR);
+               $query->execute();
+          }else{
+               echo "<script>alert(\"Adresse mail ou numéro de téléphone inconnu\")</script>";
+          }
 
-// Si le resultat de recherche n'est pas vide
-// On met a jour la table tblreaders avec le nouveau mot de passe
-// On informa l'utilisateur par une fenetre popup de la reussite ou de l'echec de l'operation
+
+
+     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +65,20 @@ include('includes/config.php');
 
      <script type="text/javascript">
           // On cree une fonction nommee valid() qui verifie que les deux mots de passe saisis par l'utilisateur sont identiques.
+          let valid = () => {
+            let message = document.getElementById("message")
+            let password = document.getElementById("password")
+            let checkPassword = document.getElementById('checkPassword')
+            if(password.value === checkPassword.value){
+            // TRUE si les mots de passe saisis dans le formulaire sont identiques
+        }
+        else{
+            // FALSE sinon
+            message.innerHTML = 'Invalide';
+            message.style.color = 'red';
+            return false;
+        }
+        }
      </script>
 
 </head>
@@ -46,9 +87,34 @@ include('includes/config.php');
      <!--On inclue ici le menu de navigation includes/header.php-->
      <?php include('includes/header.php'); ?>
      <!-- On insere le titre de la page (RECUPERATION MOT DE PASSE -->
+     <h3>RECUPERATION MOT DE PASSE</h3>
 
      <!--On insere le formulaire de recuperation-->
      <!--L'appel de la fonction valid() se fait dans la balise <form> au moyen de la propri�t� onSubmit="return valid();"-->
+     <form action="user-forgot-password.php" method="post" onSubmit="return valid();">
+          <div class="form-group">
+               <label for="email">Email</label>
+               <input type="text" name="email" required>
+          </div>
+          <div class="form-group">
+                 <label for="portable">Portable</label>
+                 <input type="text" name="portable" required>
+             </div>
+        <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <div class="form-group">
+            <label for="checkPassword">Confimez le mot de passe</label>
+            <input type="password" name="checkPassword" id="checkPassword" required><span id="message"></span>
+        </div>
+        <div class="form-group">
+            <!--A la suite de la zone de saisie du captcha, on insère l'image créée par captcha.php : <img src="captcha.php">  -->
+            <label for="vercode">Code de vérification</label>
+            <input type="text" name="vercode" required><img src="captcha.php">
+        </div>
+        <input type="submit" name="login" id="btnSubmit" class="btn btn-info" value ="Enregister"/>
+     </form>
 
 
      <?php include('includes/footer.php'); ?>
